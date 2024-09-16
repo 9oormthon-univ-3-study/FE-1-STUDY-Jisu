05f679856ee43ec40abcf7fa2ac47f6ae3a498bb
jisoong <kangjs0237@gmail.com>
2024-09-16 15:46:50 +0900

    :tada: Project setup complete

---
+++ accountbook/src/App.jsx
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
